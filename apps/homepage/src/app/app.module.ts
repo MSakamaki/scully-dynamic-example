7f90155d54ef61de546cfd779b9993c3937729c9
MSakamaki <mail@m-sakamaki.jp>
2020-10-11 12:11:11 +0900

    feature: add scully

--- apps/homepage/src/app/app.module.ts
+++ apps/homepage/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- doc/COMMAND.md
+++ doc/COMMAND.md
@@ -3,7 +3,19 @@
 ## initializeing
 
 ```sh
+# add application
 npx ng add @nrwl/angular
 npx nx g @nrwl/angular:application homepage --routing --strict
+
+# add scully
+npm install @scullyio/initgit s
+npx nx g @scullyio/init:install -- --project=homepage
+
+# build
+ng build --prod
+npm run scully
+
+# serve
+npm run scully:serve
 
 ```
